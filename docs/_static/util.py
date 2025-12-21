ce3189a89e3140217f8d23c691e77e9b433ed2a9
Ewan Short <eshort0401@gmail.com>
2025-12-21 20:16:23 +1100

    retry relative paths

--- docs/_static/util.py
+++ docs/_static/util.py
@@ -30,7 +30,7 @@ _div_close = """
     <py-script src="{python_path}" config="{config_path}"></py-script>
 </div>"""
 
-def generate_html(stub_path, html_path=None, python_path=None, config_path="/_static/assets/pyscript.toml"):
+def generate_html(stub_path, html_path=None, python_path=None, config_path="_static/assets/pyscript.toml"):
     """Generate the full HTML for a given js stub."""
     with open(stub_path, "r") as stub_file:
         stub = stub_file.read()
@@ -39,7 +39,7 @@ def generate_html(stub_path, html_path=None, python_path=None, config_path="/_st
     # Ensure the output directory exists
     Path(html_path).parent.mkdir(parents=True, exist_ok=True)
     filename = Path(stub_path).name
-    relative_path = Path("/_static")
+    relative_path = Path("_static")
     if python_path is None:
         python_path = relative_path / filename.replace(".js", ".py")
 
